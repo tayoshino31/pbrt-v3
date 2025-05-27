c595c682718178e3e28e631218891c17cd0874af
taiki <tayoshino@ucsd.edu>
2025-05-26 20:57:41 -0700

    add a lonlinear integrator

--- src/core/api.cpp
+++ src/core/api.cpp
@@ -60,6 +60,7 @@
 #include "integrators/sppm.h"
 #include "integrators/volpath.h"
 #include "integrators/whitted.h"
+#include "integrators/nonlinear.h"
 #include "lights/diffuse.h"
 #include "lights/distant.h"
 #include "lights/goniometric.h"
@@ -1691,7 +1692,10 @@ Integrator *RenderOptions::MakeIntegrator() const {
         integrator = CreateAOIntegrator(IntegratorParams, sampler, camera);
     } else if (IntegratorName == "sppm") {
         integrator = CreateSPPMIntegrator(IntegratorParams, camera);
-    } else {
+    } else if (IntegratorName == "nonlinear"){
+        integrator = CreateNonLinearIntegrator(IntegratorParams, sampler, camera);
+    }
+    else {
         Error("Integrator \"%s\" unknown.", IntegratorName.c_str());
         return nullptr;
     }
